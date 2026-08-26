51721b1e24ac6f9e7bf1abd4052b506d49510ddd
Max Suica <max.suica@gmail.com>
2026-08-26 14:39:23 -0700

    chore: consolidate up.sh & common.sh
    - Eliminate common.sh
    - Simplify up.sh
    
    We can drop all 4 environment variables from common.sh:
    
    1. DOCKER_BUILDKIT = 1 
    2. COMPOSE_DOCKER_CLI_BUILD = 1
    
    Both default as of docker engine 23 (2023).
    
    3. COMPOSE_PROJECT_NAME 
    
    This is inherited from the root directory, or can be set by adding a "name" key to compose.yml. The project does not seem to use the compose project name in any way.
    
    4. APPS_DIR
    
    Is defined in common.sh but never used in compose.yml, Dockerfile, or any other file.
    
    The remainder of common.sh are simple bash functions that could be written idiomatically with inline bash.
    
    In particularly up.sh only needs to:
    
    1. cd into project root
    2. call docker compose up --detach

--- scripts/up.sh
+++ scripts/up.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-# Creates Docker images, networks, volumes, and containers
+# Navigate to project root
+cd "$(dirname "${BASH_SOURCE[0]}")/.."
 
-# Source the shared utilities and variables from common.sh.
-# Note: Here we're sourcing 'common.sh' relative to the current script's location
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source "$SCRIPT_DIR/common.sh"
-
-# Get the script's directory, parent directory, and app name
-ROOT_DIR="$(get_parent_dir "$SCRIPT_DIR")"
-APP_NAME="payload-multi-tenant-template"
-
-echo "Starting $APP_NAME"
-
-# Define Docker Compose file path
-COMPOSE_FILE="$ROOT_DIR/compose.yml"
-
-# Run Docker Compose
-docker compose -f "$COMPOSE_FILE" up -d
+# Start docker
+echo "Starting payload-multi-tenant-template"
+docker compose up --detach
